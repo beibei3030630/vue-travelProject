5d138f5b1509aa67afbb5a123e1bd13521ba772e
beibei <1686310541@qq.com>
2019-03-13 15:17:08 +0800

    finish prepare work

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <div class="test">
+          hello world
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    .test
+        font-size 40px
+        color red
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        this is a list
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
